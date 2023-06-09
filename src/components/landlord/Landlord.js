d1c5a4314b6e2633cd9a54c89d52651ef3c7635f
Andrew Ozoria <andrewozo123@gmail.com>
2023-06-09 15:28:29 -0400

    set up provider and router

--- src/components/landlord/Landlord.js
+++ src/components/landlord/Landlord.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 function Landlord() {
   return (
     <div>Landlord</div>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import store from "./store";
+import { Provider } from "react-redux";
 import Main from "./components/Main";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
-root.render(<Main />);
+root.render(
+  <Router>
+    <Provider store={store}>
+      <Main />
+    </Provider>
+  </Router>
+);
